eaa42a71dc3f0c54a15220285a16fcc7c87fa58f
Arthur <arthur.bispo4@gmail.com>
2023-11-24 18:22:26 -0300

    Modularização da movimentação

---
+++ functions/Movement.s
@@ -0,0 +1,68 @@
+KEY:		li t1,0xFF200000		# carrega o endereco de controle do KDMMIO
+		lw t0,0(t1)			# Le bit de Controle Teclado
+		andi t0,t0,0x0001		# mascara o bit menos significativo
+   		beq t0,zero,FIM   	   	# Se nao ha tecla pressionada entao vai para FIM
+  		lw t2,4(t1)  			# le o valor da tecla tecla
+		
+		li t0,'w'
+		beq t2,t0,CHAR_CIMA		# se tecla pressionada for 'w', chama CHAR_CIMA
+		
+		li t0,'a'
+		beq t2,t0,CHAR_ESQ		# se tecla pressionada for 'a', chama CHAR_CIMA
+		
+		li t0,'s'
+		beq t2,t0,CHAR_BAIXO		# se tecla pressionada for 's', chama CHAR_CIMA
+		
+		li t0,'d'
+		beq t2,t0,CHAR_DIR		# se tecla pressionada for 'd', chama CHAR_CIMA
+	
+FIM:		ret				# retorna
+
+CHAR_ESQ:	la t0,CHAR_POS			# carrega em t0 o endereco de CHAR_POS
+		la t1,OLD_CHAR_POS		# carrega em t1 o endereco de OLD_CHAR_POS
+		lw t2,0(t0)
+		sw t2,0(t1)			# salva a posicao atual do personagem em OLD_CHAR_POS
+		
+		lh t1,0(t0)			# carrega o x atual do personagem
+		addi t1,t1,-24			# decrementa 24 pixeis
+		bltz t1, FIM			# verifica se não está tentando sair do mapa
+		sh t1,0(t0)			# salva
+		ret
+
+CHAR_DIR:	la t0,CHAR_POS			# carrega em t0 o endereco de CHAR_POS
+		la t1,OLD_CHAR_POS		# carrega em t1 o endereco de OLD_CHAR_POS
+		lw t2,0(t0)
+		sw t2,0(t1)			# salva a posicao atual do personagem em OLD_CHAR_POS
+		
+		la t0,CHAR_POS
+		lh t1,0(t0)			# carrega o x atual do personagem
+		addi t1,t1,24			# incrementa 24 pixeis
+		li t3, 296
+		bge t1, t3, FIM			# verifica se não está tentando sair do mapa
+		sh t1,0(t0)			# salva
+		ret
+
+CHAR_CIMA:	la t0,CHAR_POS			# carrega em t0 o endereco de CHAR_POS
+		la t1,OLD_CHAR_POS		# carrega em t1 o endereco de OLD_CHAR_POS
+		lw t2,0(t0)
+		sw t2,0(t1)			# salva a posicao atual do personagem em OLD_CHAR_POS
+		
+		la t0,CHAR_POS
+		lh t1,2(t0)			# carrega o y atual do personagem
+		addi t1,t1,-30			# decrementa 30 pixeis
+		bltz t1, FIM			# verifica se não está tentando sair do mapa
+		sh t1,2(t0)			# salva
+		ret
+
+CHAR_BAIXO:	la t0,CHAR_POS			# carrega em t0 o endereco de CHAR_POS
+		la t1,OLD_CHAR_POS		# carrega em t1 o endereco de OLD_CHAR_POS
+		lw t2,0(t0)
+		sw t2,0(t1)			# salva a posicao atual do personagem em OLD_CHAR_POS
+		
+		la t0,CHAR_POS
+		lh t1,2(t0)			# carrega o y atual do personagem
+		addi t1,t1,30			# incrementa 30 pixeis
+		li t3, 240
+		bge t1, t3, FIM			# verifica se não está tentando sair do mapa
+		sh t1,2(t0)			# salva
+		ret
